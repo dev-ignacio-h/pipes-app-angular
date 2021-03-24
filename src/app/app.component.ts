5e4c1957fd2c205464af2238e7a40fb36dfd64b9
Ignacio <dev.ignacio.h@gmail.com>
2021-03-23 22:18:37 -0300

    introduccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes-app-angular';
+  nombre: string = 'IgnAcio HenríqUEZ';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Ignacio',
+  };
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
